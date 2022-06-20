c7975048d2a84d9405e49c5af0dc833469d10751
Eyal Nissim <eyalnissim11@gmail.com>
2022-06-20 17:36:49 +0300

    Milestone 1

---
+++ .github/index.js
@@ -0,0 +1,7 @@
+let numInd = 8 ;
+let numFif = 13 ;
+const fibSantenc =`The Fibonacci of ${numInd} is ${numFif}`;
+document.getElementById("output").innerText= fibSantenc;
+
+//function fibonacci(){
+//}
