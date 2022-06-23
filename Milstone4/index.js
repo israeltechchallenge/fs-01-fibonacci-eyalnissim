9582569b05f0c0d176d575712cf451979b5724a5
Eyal Nissim <eyalnissim11@gmail.com>
2022-06-23 10:06:15 +0300

    Milstone4

--- Milstone4/index.js
+++ Milstone4/index.js
@@ -1,12 +1,12 @@
 const fibBtn = document.getElementById("fibCaller");
 fibBtn.addEventListener("click", fetchData);
 let fibInput = document.getElementById("input");
-function fetchData() {
-        fetch(`http://localhost:5050/fibonacci/${fibInput.value}`)
-            .then(res => res.json())
-            .then((data) => {
-                document.getElementById("output").innerText = data.result;
-            })
+function fetchData(){
+fetch(`http://localhost:5050/fibonacci/${fibInput.value}`)
+.then( res => res.json())
+.then((data) => { 
+    document.getElementById("output").innerText = data.result;
+ })
 }
 
 
